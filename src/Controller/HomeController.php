7659859ab8c6825ac5c50813448c8aca4258aae7
Timotheehau <titi.hauser@gmail.com>
2025-12-05 12:39:38 +0100

    visu connecté

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('article/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/test', name: 'test')]
+    public function test(): Response
+    {
+        return new Response('Controller chargé !');
+    }
+}
